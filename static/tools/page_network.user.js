859543a7127c39d864bbce08f05cc895c1777d77
pskdje <77605497+pskdje@users.noreply.github.com>
2025-01-16 20:48:23 +0800

    添加文章 添加2个油猴脚本

---
+++ static/tools/page_network.user.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name        允许pd网站访问其它资源
+// @namespace   https://pskdje.github.io/
+// @description 通过油猴脚本允许我网站访问其它资源
+// @author      dsjofh
+// @icon        https://pskdje.github.io/website_icon/favicon-64.ico
+// @version     2025-01-16T16
+// @match       http*://127.0.0.1:12540/*
+// @match       http*://192.168.*.*:12540/*
+// @match       https://pskdje.github.io/*
+// @run-at      document-end
+// @downloadURL https://pskdje.github.io/static/tools/page_network.user.js
+// @homepage    https://pskdje.github.io/posts/2025/01/16/page_network_userjs.html
+// @grant       GM_info
+// @grant       GM_xmlhttpRequest
+// @connect     localhost:12000
+// ==/UserScript==
+
+(function set12540websiteNetwork(){// 由于我网站的复杂性，我也没法确定要在什么时候加载
+	const tmnm="page_network.user.js";
+	const hasLWS=typeof docWindow==="function"&&typeof docsScript==="object";// 检查是否有我网站的接口
+	if(!hasLWS)console.warn("未运行于特定环境，部分逻辑可能会无法运行。");
+	const path=location.pathname,
+		tmsou=`${tmnm}>`;
+	cache.log.root.add(20,tmnm,`油猴脚本"${GM_info.script.name}"已在"${path}"区域加载。`);
+	function on_toHTTPAgent(ev){// toHTTPAgent代理的油猴实现
+		const d=ev.data;
+		const q=d.data;
+		function pd(t,r,s){
+			ev.target.post("SHTTP",{id:d.id,type:t,response:r},s);
+		}
+		function rj(){pd("reject")}
+		function sph(ht){
+			if(typeof ht!=="string")return ht;
+			const r={};
+			for(const d in ht.split("\r\n")){
+				const h=d.split(":",2);
+				r[h[0].trim()]=h[1].trim();
+			};return r;
+		}
+		GM_xmlhttpRequest({
+			method:q.method,
+			url:q.url,
+			headers:q.headers,
+			data:q.body,
+			responseType:"arraybuffer",
+			timeout:50000,
+			onabort:rj,onerror:rj,ontimeout:rj,
+			onload(r){
+				pd("response",{body:r.response,status:r.status,statusText:r.statusText,headers:sph(r.responseHeaders)});
+			}
+		});
+	}
+	if(path==="/assets/audioplay.html"){
+		deval.on_toHTTPAgent=on_toHTTPAgent;
+	}
+})();
---
+++ static/tools/tm_js/no_bpbp_sac_df_req.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         拦截哔哩哔哩搜索框默认文本
+// @namespace    https://pskdje.github.io/static/assets/tools/tm_js/
+// @version      0.0.1
+// @description  通过拦截请求来阻止哔哩哔哩**引战搜索框文字
+// @author       dsjofh
+// @match        http*://*.bilibili.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @run-at       document-start
+// @grant        none
+// @require      https://scriptcat.org/lib/637/1.4.3/ajaxHooker.js#sha256=y1sWy1M/U5JP1tlAY5e80monDp27fF+GMRLsOiIrSUY=
+// @downloadURL  https://pskdje.github.io/static/assets/tools/tm_js/no_bpbp_sac_df_req.user.js
+// @license      CC0
+// @noframes
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	ajaxHooker.filter([// 注意: 使用的库对url的过滤是相对url
+		{url:/(?:https?\:)?\/\/api\.bilibili\.com\/\/?x\/web-interface\/(?:wbi\/)?search\/default/}
+	]);
+	ajaxHooker.hook(req=>{
+		req.abort=true;
+		console.info("Abort: ",req.url);
+		//ajaxHooker.unhook();
+	});
+})();
